4a184f17475d8c60a8d12c6fbe7511e4180a5a05
lopezrunco <lopezrunco@gmail.com>
2022-02-15 14:40:23 +0530

    Seeder generates posts

--- src/commands/seeder.js
+++ src/commands/seeder.js
@@ -5,12 +5,15 @@ const faker = require('faker')
 const mongoose = require('mongoose')
 const getDbConnectionString = require('../utils/get-db-connection-string')
 const { userModel } = require('../models/user')
+const { postModel } = require('../models/post')
 
 const users = []
 const userPassword = bcrypt.hashSync('super_mega_secret', 2)
 const numberOfUsers = 10
+const posts = []
+const numberOfPosts = 20
 
-for (let iterationIndex = 0; iterationIndex < numberOfUsers; iterationIndex++) {
+for (let generateUsersIndex = 0; generateUsersIndex < numberOfUsers; generateUsersIndex++) {
     users.push({
         name: faker.internet.userName(),
         email: faker.internet.email(),
@@ -20,14 +23,63 @@ for (let iterationIndex = 0; iterationIndex < numberOfUsers; iterationIndex++) {
     })
 }
 
+for (let generatePostsIndex = 0; generatePostsIndex < numberOfPosts; generatePostsIndex++) {
+    posts.push({
+        title: faker.name.title(),
+        published: true,
+        category: faker.lorem.word(),
+        tags: [
+            faker.lorem.word(),
+            faker.lorem.word(),
+            faker.lorem.word()
+        ],
+        primaryImageUrl: faker.image.image(),
+        content: [
+            faker.lorem.paragraph(),
+            faker.lorem.paragraph(),
+            faker.lorem.paragraph(),
+            faker.lorem.paragraph(),
+            faker.lorem.paragraph(),
+            faker.lorem.paragraph(),
+            faker.lorem.paragraph(),
+            {
+                subtitle: faker.name.title(),
+                subcontent: [
+                    faker.lorem.paragraph(),
+                    faker.lorem.paragraph(),
+                    faker.lorem.paragraph()
+                ]
+            },
+            {
+                subtitle: faker.name.title(),
+                subcontent: [
+                    faker.lorem.paragraph(),
+                    faker.lorem.paragraph(),
+                ]
+            }
+        ],
+        secondaryImagesUrls: [
+            faker.image.image(),
+            faker.image.image(),
+            faker.image.image()
+        ],
+        links: [
+            faker.internet.domainName(),
+            faker.internet.domainName()
+        ],
+        author: faker.name.firstName()
+    })
+}
+
 console.log('------------------------------------------------------------------------')
 console.log('Seeder running')
-console.log(`Will be inserted ${numberOfUsers} users`)
+console.log(`Will be inserted ${numberOfUsers} users and ${numberOfPosts} posts`)
 
 mongoose.connect(getDbConnectionString(), { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         Promise.all([
-            userModel.insertMany(users)
+            userModel.insertMany(users),
+            postModel.insertMany(posts)
         ]).then(() => {
             console.log('Done!')
             console.log('------------------------------------------------------------------------')
---
+++ src/models/post.js
@@ -0,0 +1,51 @@
+const { model, Schema } = require('mongoose')
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    published: {
+        type: Boolean,
+        required: true,
+        default: true
+    },
+    category: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    tags: {
+        type: Array,
+        default: []
+    },
+    primaryImageUrl: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content: {
+        type: Array,
+        default: []
+    },
+    secondaryImagesUrls: {
+        type: Array,
+        default: []
+    },
+    links: {
+        type: Array,
+        default: []
+    },
+    author: {
+        type: String,
+        trim: true
+    }
+})
+
+const postModel = model('posts', postSchema)
+
+module.exports = {
+    postSchema,
+    postModel
+}
